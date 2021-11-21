5a5d5c3397da81d64f4a75e55c305ddc36c49bb8
Gkkf <gkkf123@o2.pl>
2021-11-21 11:22:37 +0100

    MusicBot

--- index.js
+++ index.js
@@ -5,10 +5,34 @@ DisTube = require('distube');
 
 client.distube = new DisTube(client, { searchSongs: false, emitNewSongOnly: true });
 
+    function embed()
+    {
+        let n = new MessageEmbed()
+        .setTitle("Teraz gram:")
+        .setDescription(`${song.name}\n\`Wywołane przez:\` ${song.user}`)
+        .setColor("#ff0000")
+        .setThumbnail('https://img.icons8.com/clouds/2x/play.png')
+        .addField(
+            "\u200b",
+            new Date(seek * 1000).toISOString().substr(11, 8) +
+            "[ " +
+            createBar(song.formattedDuration == 0 ? seek : song.formattedDuration, seek, 10)[0] +
+            "] " +
+            (song.formattedDuration == 0 ? " ◉ LIVE" : new Date(song.formattedDuration * 1000).toISOString().substr(11, 8)),
+            false
+        )
+
+        if(song.formattedDuration > 0)
+        {
+            n.setFooter("Pozostało: " + new Date(left * 1000).toISOString().substr(11, 8));
+        }
+    }
+
 client.distube
-    .on("playSong", (message, queue, song) => message.channel.send(
-        `Teraz gram: \`${song.name}\` - \`${song.formattedDuration}\`\nWywołane przez: ${song.user}`
-	))
+    .on("playSong", (message, queue, song) => message.channel.send( embed()     
+        //`Teraz gram: \`${song.name}\` - \`${song.formattedDuration}\`\nWywołane przez: ${song.user}`
+    ))
+
 	.on("addSong", (message, queue, song) => message.channel.send(
         `Dodano: ${song.name} - \`${song.formattedDuration}\` do kolejki odtwarzania przez: ${song.user}`
     ))
