67ccb25a3ccd3fd4d2634d4a649c033ebb2d830e
Gkkf <gkkf123@o2.pl>
2021-11-21 11:31:44 +0100

    MusicBot

--- index.js
+++ index.js
@@ -6,7 +6,33 @@ DisTube = require('distube');
 client.distube = new DisTube(client, { searchSongs: false, emitNewSongOnly: true });
 
 client.distube
-    .on("playSong", (message, queue, song) => message.channel.send( embed()     
+    .on("playSong", (message, queue, song) => message.channel.send( 
+    function embed()
+    {
+        let n = new Discord.MessageEmbed()
+        .setTitle("Teraz gram:")
+        .setDescription(`${song.name}\n\`Wywołane przez:\` ${song.user}`)
+        .setColor("#ff0000")
+        .setThumbnail('https://img.icons8.com/clouds/2x/play.png')
+        .addField(
+            "\u200b",
+            new Date(seek * 1000).toISOString().substr(11, 8) +
+            "[ " +
+            createBar(song.formattedDuration == 0 ? seek : song.formattedDuration, seek, 10)[0] +
+            "] " +
+            (song.formattedDuration == 0 ? " ◉ LIVE" : new Date(song.formattedDuration * 1000).toISOString().substr(11, 8)),
+            false
+        )
+
+        if(song.formattedDuration > 0)
+        {
+            n.setFooter("Pozostało: " + new Date(left * 1000).toISOString().substr(11, 8));
+
+            return message.channel.send(n);
+        }
+    }
+
+    , embed()
         //`Teraz gram: \`${song.name}\` - \`${song.formattedDuration}\`\nWywołane przez: ${song.user}`
     ))
 
@@ -64,31 +90,6 @@ client.on('message', async message =>
         const music = args.join(" ");
 
         if(!music) return;
-
-        function embed()
-        {
-        let n = new Discord.MessageEmbed()
-        .setTitle("Teraz gram:")
-        .setDescription(`${song.name}\n\`Wywołane przez:\` ${song.user}`)
-        .setColor("#ff0000")
-        .setThumbnail('https://img.icons8.com/clouds/2x/play.png')
-        .addField(
-            "\u200b",
-            new Date(seek * 1000).toISOString().substr(11, 8) +
-            "[ " +
-            createBar(song.formattedDuration == 0 ? seek : song.formattedDuration, seek, 10)[0] +
-            "] " +
-            (song.formattedDuration == 0 ? " ◉ LIVE" : new Date(song.formattedDuration * 1000).toISOString().substr(11, 8)),
-            false
-        )
-
-        if(song.formattedDuration > 0)
-        {
-            n.setFooter("Pozostało: " + new Date(left * 1000).toISOString().substr(11, 8));
-
-            return message.channel.send(n);
-        }
-        }
 
         await client.distube.play(message, music);
 
